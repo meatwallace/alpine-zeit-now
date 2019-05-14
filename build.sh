8f816fac9ae381a985e4b038237c43eacb1287b8
Geoff Whatley <me@geoffwhatley.com>
2019-05-15 00:32:12 +1000

    ci(#7): verify the image contains the correct `now` version
    - rewrote the build script to include a sanity check/verification step
      that validate the `now` version in the container matches the one we
      expect
    - enforced a strict commit message format by pulling the `now` version
      we want from the commit message itself
    - snuck in a shellcheck linting stage in the CI pipeline to ensure our
      scripts are in order pre-merge
    
    closes #7

--- build.sh
+++ build.sh
@@ -1,27 +1,54 @@
 #!/bin/sh
 
-set -euo pipefail
-
-NODEJS_VERSION="$NODEJS_LTS_VERSION"
-NOW_CLI_VERSION="$GIT_BRANCH"
-IMAGE_TAG="$DOCKER_USER/alpine-zeit-now"
+set -eu
 
 echo "$DOCKER_PASS" | docker login --username "$DOCKER_USER" --password-stdin
 
+# the version extraction logic is brittle as it relies on extracting the end of
+# the commit message, however we're able to enforce a strict commit message
+# format as a side effect:
+# release(#<issue_no>): meatwallace/alpine-zeit-now:<now_version>
+COMMIT_MESSAGE="$(git log --pretty=oneline -n 1 HEAD)"
+# as our image tag contains slashes, opted to use | as our seperator for sed
+EXPECTED_NOW_VERSION="$(echo "$COMMIT_MESSAGE" | sed -E "s|release\(#[0-9]+\): $IMAGE_TAG:||")"
+
 docker build . \
   --build-arg="CACHE_BUSTER=$(date +%s)" \
-  --build-arg="NOW_CLI_VERSION=$NOW_CLI_VERSION" \
   --build-arg="NODEJS_VERSION=$NODEJS_VERSION" \
+  --build-arg="NOW_CLI_VERSION=$EXPECTED_NOW_VERSION" \
   --tag="$IMAGE_TAG:latest"
 
-docker tag "$IMAGE_TAG:latest" "$IMAGE_TAG:$NOW_CLI_VERSION"
+# run the container and print the `now` version to sanity check that we've
+# built an image that runs with the correct version
+ACTUAL_NOW_VERSION="$(docker run "$IMAGE_TAG" now --version)"
 
-docker push "$IMAGE_TAG:$NOW_CLI_VERSION"
-docker push "$IMAGE_TAG:latest"
+# verify that the versions match, otherwise bail out
+if [ "$ACTUAL_NOW_VERSION" != "$EXPECTED_NOW_VERSION" ]; then
+  echo "expected now@$EXPECTED_NOW_VERSION but received now@$ACTUAL_NOW_VERSION"
+  
+  exit 1
+fi
 
-SIZE_BYTES="$(docker image inspect $IMAGE_TAG:latest --format='{{.Size}}')"
-SIZE_MB="$(printf "%.2f" "$((10**9 * SIZE_BYTES / 1000000))e-9")"
+# determine and print out the uncompressed size which we'll add to our README,
+# which sadly requires a custom function to be POSIX compliant
+pow() {
+  set "$1" "$2" 1
+  while [ "$2" -gt 0 ]; do
+    set "$1" $(($2-1)) $(($1*$3))
+  done
+  printf "%d\n" "$3"
+}
+
+SIZE_BYTES="$(docker image inspect "$IMAGE_TAG:latest" --format='{{.Size}}')"
+SIZE_MB="$(printf "%.2f" "$(($(pow 10 9) * SIZE_BYTES / 1000000))e-9")"
 
 printf "%s\\n" "$IMAGE_TAG:$NOW_CLI_VERSION"
 printf "%s: %s%s\\n" "size(uncompressed)" "$SIZE_MB" "MB"
 
+# tag the image with the appropriate version and push it if we're on master
+if [ "$GIT_BRANCH" = "master" ]; then
+  docker tag "$IMAGE_TAG:latest" "$IMAGE_TAG:$ACTUAL_NOW_VERSION"
+  
+  docker push "$IMAGE_TAG:$ACTUAL_NOW_VERSION"
+  docker push "$IMAGE_TAG:latest"
+fi
