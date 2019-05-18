71eca4958d7430b3793ba6fe13978e4b7fc0f14b
Geoff Whatley <me@geoffwhatley.com>
2019-05-18 17:41:45 +1000

    release(#9): meatwallace/alpine-zeit-now:15.2.1-canary.1
    closes #9

--- README.md
+++ README.md
@@ -9,10 +9,19 @@ slim image as a base.
 Images are available at the
 [meatwallace/alpine-zeit-now](https://hub.docker.com/r/meatwallace/alpine-zeit-now)
 repository, tagged `meatwallace/alpine-zeit-now:<now-version>`. All versions
-\>=15.2.0 including canary releases should be available.
+\>=15.2.0 including canary releases should be available, however only the most
+recent 20 of each will be listed below.
+
+Stable releases:
 
 | now-version     | nodejs-version | size (un/compressed) |
 |-----------------|----------------|----------------------|
-| 15.2.1-canary.0 | 10.15.3        | 94.2mb / 36mb        |
 | 15.2.0          | 10.15.3        | 94.2mb / 36mb        |
+
+Canary releases:
+
+| now-version     | nodejs-version | size (un/compressed) |
+|-----------------|----------------|----------------------|
+| 15.2.1-canary.1 | 10.15.3        | 95.2mb / 36mb        |
+| 15.2.1-canary.0 | 10.15.3        | 94.2mb / 36mb        |
 | 15.1.1-canary.4 | 10.15.3        | 94.2mb / 36mb        |
--- build.sh
+++ build.sh
@@ -8,9 +8,11 @@ echo "$DOCKER_PASS" | docker login --username "$DOCKER_USER" --password-stdin
 # the commit message, however we're able to enforce a strict commit message
 # format as a side effect:
 # release(#<issue_no>): meatwallace/alpine-zeit-now:<now_version>
-COMMIT_MESSAGE="$(git log --pretty=oneline -n 1 HEAD)"
+COMMIT_MESSAGE="$(git log --format="%B" -n 1 HEAD | head -n 1)"
 # as our image tag contains slashes, opted to use | as our seperator for sed
 EXPECTED_NOW_VERSION="$(echo "$COMMIT_MESSAGE" | sed -E "s|release\(#[0-9]+\): $IMAGE_TAG:||")"
+
+echo "Building meatwallace/alpine-zeit-now:$EXPECTED_NOW_VERSION"
 
 docker build . \
   --build-arg="CACHE_BUSTER=$(date +%s)" \
@@ -25,16 +27,18 @@ ACTUAL_NOW_VERSION="$(docker run "$IMAGE_TAG" now --version)"
 # verify that the versions match, otherwise bail out
 if [ "$ACTUAL_NOW_VERSION" != "$EXPECTED_NOW_VERSION" ]; then
   echo "expected now@$EXPECTED_NOW_VERSION but received now@$ACTUAL_NOW_VERSION"
-  
+
   exit 1
 fi
+
+NOW_CLI_VERSION="$ACTUAL_NOW_VERSION"
 
 # determine and print out the uncompressed size which we'll add to our README,
 # which sadly requires a custom function to be POSIX compliant
 pow() {
   set "$1" "$2" 1
   while [ "$2" -gt 0 ]; do
-    set "$1" $(($2-1)) $(($1*$3))
+    set "$1" $(($2 - 1)) $(($1 * $3))
   done
   printf "%d\n" "$3"
 }
@@ -42,13 +46,13 @@ pow() {
 SIZE_BYTES="$(docker image inspect "$IMAGE_TAG:latest" --format='{{.Size}}')"
 SIZE_MB="$(printf "%.2f" "$(($(pow 10 9) * SIZE_BYTES / 1000000))e-9")"
 
-printf "%s\\n" "$IMAGE_TAG:$NOW_CLI_VERSION"
-printf "%s: %s%s\\n" "size(uncompressed)" "$SIZE_MB" "MB"
+printf '%s\n\n' "$IMAGE_TAG:$NOW_CLI_VERSION"
+printf '%s: %s%s\n' "size(uncompressed)" "$SIZE_MB" "MB"
 
 # tag the image with the appropriate version and push it if we're on master
 if [ "$GIT_BRANCH" = "master" ]; then
   docker tag "$IMAGE_TAG:latest" "$IMAGE_TAG:$ACTUAL_NOW_VERSION"
-  
+
   docker push "$IMAGE_TAG:$ACTUAL_NOW_VERSION"
   docker push "$IMAGE_TAG:latest"
 fi
